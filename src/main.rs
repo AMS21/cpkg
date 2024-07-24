8404865d773f50df0044d32a376780cd5d03377d
AMS21 <AMS21.github@gmail.com>
2024-07-24 16:34:21 +0200

    Small code simplification

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,7 @@ fn main() -> Result<()> {
     color_eyre::install()?;
 
     // Handle command line arguments
-    let command_line = cli::get_command_line();
-    let matches = command_line.get_matches();
+    let matches = cli::get_command_line().get_matches();
 
     match matches.subcommand() {
         Some((install::SUBCOMMAND_NAME, sub_matches)) => {
