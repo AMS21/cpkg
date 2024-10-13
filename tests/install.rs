0f6c1703686a856e45ff6e8e44858c0358a79e2c
AMS21 <AMS21.github@gmail.com>
2024-10-13 16:21:03 +0200

    Add required info to make the crate publishable

--- tests/install.rs
+++ tests/install.rs
@@ -8,7 +8,7 @@ fn install_non_existing_package() {
         return;
     }
 
-    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let mut cmd = Command::cargo_bin("cpkg").unwrap();
 
     cmd.arg("install");
     cmd.arg("This-Package-Should-Not-Exists");
