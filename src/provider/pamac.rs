ca9059667cd1778a2ec59f43c30008ed1b6ac2bd
AMS21 <AMS21.github@gmail.com>
2024-10-14 11:03:59 +0200

    refactor: Simplify provider command implementations

--- src/provider/pamac.rs
+++ src/provider/pamac.rs
@@ -7,12 +7,52 @@ use crate::utility::command_to_full_string;
 use std::path::PathBuf;
 
 // TODO: Instead of installed boolean just have executable_path as Option
-// TODO: Generalize a lot of this maybe with a macro?
 
 #[allow(clippy::module_name_repetitions)]
 pub struct PamacProvider {
     executable_path: PathBuf,
     installed: bool,
+}
+
+impl PamacProvider {
+    fn run_command(
+        &self,
+        command_name: &str,
+        packages: &[String],
+        assume_yes: bool,
+        dry_run: bool,
+    ) -> Result<()> {
+        let mut command = std::process::Command::new(&self.executable_path);
+
+        command.arg(command_name);
+
+        // Now add all the translated package names
+        for package in packages {
+            command.arg(package);
+        }
+
+        // Handle assume yes
+        if assume_yes {
+            command.arg("--no-confirm");
+        }
+
+        // Handle dry run
+        if dry_run {
+            command.arg("-d");
+        }
+
+        // Run the actual command
+        let return_code = command.spawn()?.wait()?;
+
+        if !return_code.success() {
+            return Err(Error::CommandFailed {
+                exit_code: return_code,
+                command_line: command_to_full_string(&command)?,
+            });
+        }
+
+        Ok(())
+    }
 }
 
 impl Provider for PamacProvider {
@@ -53,69 +93,11 @@ impl Provider for PamacProvider {
     }
 
     fn install_packages(&self, packages: &[String], options: &install::Options) -> Result<()> {
-        let mut command = std::process::Command::new(&self.executable_path);
-
-        command.arg("install");
-
-        // Now add all the translated package names
-        for package in packages {
-            command.arg(package);
-        }
-
-        // Handle assume yes
-        if options.assume_yes {
-            command.arg("--no-confirm");
-        }
-
-        // Handle dry run
-        if options.dry_run {
-            command.arg("-d");
-        }
-
-        // Run the actual command
-        let return_code = command.spawn()?.wait()?;
-
-        if !return_code.success() {
-            return Err(Error::InstallCommandFailed {
-                exit_code: return_code,
-                command_line: command_to_full_string(&command)?,
-            });
-        }
-
-        Ok(())
+        self.run_command("install", packages, options.assume_yes, options.dry_run)
     }
 
     fn remove_packages(&self, packages: &[String], options: &remove::Options) -> Result<()> {
-        let mut command = std::process::Command::new(&self.executable_path);
-
-        command.arg("remove");
-
-        // Now add all the translated package names
-        for package in packages {
-            command.arg(package);
-        }
-
-        // Handle assume yes
-        if options.assume_yes {
-            command.arg("--no-confirm");
-        }
-
-        // Handle dry run
-        if options.dry_run {
-            command.arg("-d");
-        }
-
-        // Run the actual command
-        let return_code = command.spawn()?.wait()?;
-
-        if !return_code.success() {
-            return Err(Error::RemoveCommandFailed {
-                exit_code: return_code,
-                command_line: command_to_full_string(&command)?,
-            });
-        }
-
-        Ok(())
+        self.run_command("remove", packages, options.assume_yes, options.dry_run)
     }
 
     fn reinstall_packages(
@@ -123,33 +105,7 @@ impl Provider for PamacProvider {
         packages: &[String],
         options: &crate::subcommand::reinstall::Options,
     ) -> Result<()> {
-        let mut command = std::process::Command::new(&self.executable_path);
-
-        command.arg("reinstall");
-
-        // Now add all the translated package names
-        for package in packages {
-            command.arg(package);
-        }
-
-        // Handle assume yes
-        if options.assume_yes {
-            command.arg("--no-confirm");
-        }
-
-        // Handle dry run
-        // NOTE: pamac reinstall can't handle dry run
-
-        // Run the actual command
-        let return_code = command.spawn()?.wait()?;
-
-        if !return_code.success() {
-            return Err(Error::RemoveCommandFailed {
-                exit_code: return_code,
-                command_line: command_to_full_string(&command)?,
-            });
-        }
-
-        Ok(())
+        // NOTE: pamac's reinstall can't handle dry run
+        self.run_command("reinstall", packages, options.assume_yes, false)
     }
 }
